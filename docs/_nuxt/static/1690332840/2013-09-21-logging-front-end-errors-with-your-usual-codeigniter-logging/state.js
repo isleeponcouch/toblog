318bd3dab9e5f64e49ad6d3569213a4152148d65
Tobias Gundry <tobygundry@gmail.com>
2023-07-26 10:54:07 +1000

    Add some more tailwind safelists

---
+++ docs/_nuxt/static/1690332840/2013-09-21-logging-front-end-errors-with-your-usual-codeigniter-logging/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1690332840",layout:"default",error:a,serverRendered:true,routePath:"\u002F2013-09-21-logging-front-end-errors-with-your-usual-codeigniter-logging",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8d427149"}}}}(null));
