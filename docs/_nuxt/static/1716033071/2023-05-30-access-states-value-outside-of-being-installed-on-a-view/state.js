b8020014f50f22dd22cbe8a60d4fcb372f51199c
Tobias Gundry <tobygundry@gmail.com>
2024-05-18 21:55:56 +1000

    Style update

--- docs/_nuxt/static/1716033071/2023-05-30-access-states-value-outside-of-being-installed-on-a-view/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1716033071",layout:"default",error:a,serverRendered:true,routePath:"\u002F2023-05-30-access-states-value-outside-of-being-installed-on-a-view",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dd7a4846"}}}}(null));
--- pages/_slug.vue
+++ pages/_slug.vue
@@ -30,7 +30,7 @@
        <p class="md:font-medium text-sm md:text-base rounded inline-block p-1" v-if="article.createdAt != article.updatedAt" v-bind:class="article.bg">First published {{ formatDate(article.createdAt) }}, updated {{ formatDate(article.updatedAt) }}</p>
       </div>
     </header>
-    <div v-if="article.archived" class="max-w-2xl mx-auto space-y-6 border-t-4 rounded-b px-4 py-3 shadow-md my-3" role="alert">
+    <div v-if="article.archived" class="max-w-2xl mx-auto space-y-6 border-t-4 rounded-b px-4 py-3 shadow-md my-3 bg-gray-600" role="alert">
       <div class="flex">
         <div class="py-1"><svg class="fill-current h-6 w-6 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
         <div>
