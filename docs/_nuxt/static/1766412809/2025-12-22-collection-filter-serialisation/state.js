9908812c88909512e79344044cb118f36e970cf4
Tobias Gundry <tobygundry@gmail.com>
2026-01-01 13:33:14 +1000

    Generate

--- docs/_nuxt/static/1766412809/2025-12-22-collection-filter-serialisation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1766412809",layout:"default",error:a,serverRendered:true,routePath:"\u002F2025-12-22-collection-filter-serialisation",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ef3745ed"}}}}(null));
--- docs/_nuxt/static/1766412809/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1766412809",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ef3745ed"}}}}(null,"\u002F"));
